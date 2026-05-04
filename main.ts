7e409582f2c194dd1c138b6484364384031be234
AmirMh36 <78902868+AmirMh36@users.noreply.github.com>
2026-05-04 12:35:44 +0330

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
-import { serveFile } from "jsr:@std/http/file-server";
+const BACKEND = "http://YOUR_SERVER_IP:8080";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+Deno.serve(async (req) => {
+  const url = new URL(req.url);
+
+  const target = new URL(url.pathname + url.search, BACKEND);
+
+  const headers = new Headers(req.headers);
+  headers.delete("host");
+  headers.delete("connection");
+
+  const res = await fetch(target.toString(), {
+    method: req.method,
+    headers,
+    body: req.body,
+  });
+
+  return new Response(res.body, {
+    status: res.status,
+    headers: res.headers,
+  });
 });
